928ed9aa8537d73263792eb2daf4d568d9cbf8e4
侯伟轩 <wxhou@yunjinginc.com>
2020-08-28 15:17:23 +0800

    fix bug

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,7 +30,7 @@ def pytest_collection_modifyitems(items):
     """
     for item in items:
         item.name = item.name.encode("utf-8").decode("unicode_escape")
-        item._nodeid = item.nodeid.encode("utf-8").decode("unicode_escape") %   
+        item._nodeid = item.nodeid.encode("utf-8").decode("unicode_escape")
 
 
 if __name__ == '__main__':
